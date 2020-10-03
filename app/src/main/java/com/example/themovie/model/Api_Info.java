0df0b96de28c545c25f1ddc8d3c1c84523cf2ca8
guigarcia96 <gmoraes1@outlook.com>
2020-10-03 15:41:29 -0300

    Removing key

--- app/src/main/java/com/example/themovie/model/Api_Info.java
+++ app/src/main/java/com/example/themovie/model/Api_Info.java
@@ -2,7 +2,7 @@ package com.example.themovie.model;
 
 public class  Api_Info {
      public String URL_BASE = "https://api.themoviedb.org/3";
-     public String API_KEY = "6dac0fd5c85431d1c5643511b0f35c71";
+     public String API_KEY = "";
      public String LANGUAGE = "pt-BR" ;
 
 
